7227c9055296e16cbf32db1008369f2a087ea9eb
Sara Doral <sara.doral@educa.madrid.org>
2026-03-05 16:49:50 +0100

    Añadiendo el último ejercicio del primer tema (primer array)

---
+++ 02-ejercicios-daw/tem1/js/ejercicio9.js
@@ -0,0 +1,8 @@
+//Creación variable semana
+let semana=["Lunes", "Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
+
+//Mostramos en el html, el contenido
+document.write("Días de la semana; ", semana);
+
+//Procedemos a mostrar los días que nos indican:
+document.write("<br>Mis días de la semana favoritos: ", semana[4]," - " ,semana[5], " - ",semana[6]);
