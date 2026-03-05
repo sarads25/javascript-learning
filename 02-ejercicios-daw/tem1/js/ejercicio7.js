bacda7b6176eeac781a4efe389cb26af10db1ea9
Sara Doral <sara.doral@educa.madrid.org>
2026-03-05 16:29:09 +0100

    Realizado ejercicio 7 de incrementación

---
+++ 02-ejercicios-daw/tem1/js/ejercicio7.js
@@ -0,0 +1,17 @@
+//Creación de variable
+let num;
+
+//Pedir al usuario numero
+num=parseInt(prompt("Introduzca un número"));
+
+//Mostramos resultados aplicando la incrementación del núm
+
+document.write("El resultado de incrementar el numero es: ", num++);
+document.write("El resultado de incrementar de nuevo el numero es: ", ++num);
+document.write("El valor actual es: ", num);
+
+/*let num = parseInt(prompt("Introduce un número"));
+
+document.write("<br>Incremento con num++: " + num++);
+document.write("<br>Incremento con ++num: " + ++num);
+document.write("<br>Valor actual del número: " + num); */
